ff2cd7560c620a53b2ea26efe446d9d7448bed62
SimpleMan2022 <aditn689@gmail.com>
2024-05-07 19:52:09 +0700

    fix: separate router for clean code

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"evoting/config"
 	"evoting/routers"
-	"evoting/routers/admin"
-	"evoting/routers/user"
 	"fmt"
 	"github.com/labstack/echo/v4"
 )
@@ -15,37 +13,7 @@ func main() {
 	e := echo.New()
 
 	e.Static("/images", "public/images")
-	v1User := e.Group("/api/v1")
-	{
-		userAddress := v1User.Group("/profile")
-		user.UserAddressRouter(userAddress)
-
-		places := v1User.Group("/places")
-		user.PlaceRouter(places)
-
-		votes := v1User.Group("/votes")
-		user.VoteRouter(votes)
-	}
-
-	v1Admin := e.Group("/api/v1/admin")
-	{
-		auth := v1Admin.Group("/auth")
-		routers.AuthRouter(auth)
-
-		users := v1Admin.Group("/users")
-		admin.UserRouter(users)
-
-		places := v1Admin.Group("/places")
-		admin.PlaceRouter(places)
-
-		weather := v1Admin.Group("/places")
-		admin.WeatherRouter(weather)
-
-		placeAddress := v1Admin.Group("/places/address")
-		admin.PlaceAddressRouter(placeAddress)
-
-	}
-
+	routers.SetupRouter(e)
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", config.ENV.PORT)))
 
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%v", config.ENV.PORT)))
